54bca2cff3e81a8bb96ae24ad9388133a5a269c8
codepanther <pezechigozirim@gmail.com>
2023-01-03 18:21:28 +0100

    Implement search feature and display results in SearchFeed component

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,21 @@
 import { Search } from '@mui/icons-material';
 import { IconButton, Paper } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchTerm) navigate(`search/${searchTerm}`)
+  }
+
   return (
     <Paper
       component="form"
-      onSubmit={() => {}}
+      onSubmit={handleSubmit}
       sx={{
         borderRadius: 20,
         border: '1px solid #e3e3e3',
@@ -18,8 +27,8 @@ const SearchBar = () => {
       <input
         className="search-bar"
         placeholder="Search..."
-        value=""
-        onchange={() => {}}
+        value={searchTerm}
+        onChange={(e) => {setSearchTerm(e.target.value)}}
       />
       <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
         <Search />
