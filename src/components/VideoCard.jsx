d8c157af095e6a72301b432a85f375f0d2a6de69
codepanther <pezechigozirim@gmail.com>
2023-01-03 16:28:20 +0100

    Fix the white background bug showing on the Card image

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -29,7 +29,7 @@ const VideoCard = ({
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: '100%', height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }}>
