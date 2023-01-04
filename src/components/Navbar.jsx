9547cf49ae8aba6e5d0c105eeb3e4c2747d9b4e5
codepanther <pezechigozirim@gmail.com>
2023-01-04 10:58:38 +0100

    set z-index of navbar so that scrolling doesn't obstruct it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,8 @@ const Navbar = () => {
         position: 'sticky',
         background: '#000',
         top: 0,
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        zIndex: 1
       }}
     >
       <Link to="/" style={{ display: 'flex', alignItems: 'center' }}>
